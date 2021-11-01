bee181f21a8a2552217db00ef3628ef45893b26e
Julie Miguel <pascalinem660@gmail.com>
2021-11-02 01:15:20 +0400

    event listener

---
+++ main.js
@@ -0,0 +1,5 @@
+const collection = document.querySelector('.dropbtn');
+collection.addEventListener('click', (event) => {
+  const droplist = document.querySelector(".dropdown-content-left");
+  droplist.style.display = "";
+});

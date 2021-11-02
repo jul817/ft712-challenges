d5bd23425001d6f5c62adbba964ccced52a490f8
Julie Miguel <pascalinem660@gmail.com>
2021-11-02 22:26:05 +0400

    collections images refactoring

--- main.js
+++ main.js
@@ -20,8 +20,8 @@ collection.addEventListener('click', (event) => {
   // let state = droplist.style.display;
   // let droplist = document.querySelector(".dropdown-content-left");
   // if (droplist.style.display = "none"){
-    const droplistcollection = document.querySelector(".dropdown-content-left");
-    droplistcollection.classList.toggle("show");
+    const dropListCollection = document.querySelector(".dropdown-content-left");
+    dropListCollection.classList.toggle("show");
    // console.log(droplist.style.display);
     // droplist.style.display = "flex";
     // console.log(droplist.style.display);
@@ -36,14 +36,92 @@ collection.addEventListener('click', (event) => {
 // Contact Us dropdown content
 const contact = document.querySelector('#dropbtn-right');
 contact.addEventListener('click', (event) => {
-  const droplistcontact = document.querySelector(".dropdown-content-right");
+  const dropListContact = document.querySelector(".dropdown-content-right");
 
-    droplistcontact.classList.toggle("show");
+    dropListContact.classList.toggle("show");
 
 });
 
+// Collection images
 
-const collectionimage = document.querySelector('#autumn-collection');
-collectionimage.addEventListener('click', (event) => {
-  alert('Hello');
+
+
+
+const toggleShow = () => {
+  const collectionNameImage = document.getElementById(`image-${collectionNameText}`);
+  collectionNameImage.classList.toggle("show");
+};
+
+
+const collectionSummer = document.getElementById('summer-collection');
+const collectionWinter = document.getElementById('winter-collection');
+const collectionAutumn = document.getElementById('autumn-collection');
+const collectionList = document.querySelectorAll('.collection');
+// console.log(collectionName.text.toLowerCase());
+collectionList.forEach(element => {
+  const collectionName = document.getElementById(`${element.id}`);
+  console.log(collectionName);
+  const collectionNameText = collectionName.text.toLowerCase();
+  element.addEventListener('click', (event) => {
+    console.log(collectionNameText);
+    const collectionNameImage = document.getElementById(`image-${collectionNameText}`);
+    collectionNameImage.classList.toggle("show");
+  });
+ // const collectionNameText = collectionName.text.toLowerCase();
+   // console.log(collectionNameText);
 });
+
+
+
+// collectionName.addEventListener('click', toggleShow);
+// const collectionNameText = collectionName.text.toLowerCase();
+
+
+
+// console.log(collectionNameText);
+
+
+
+// => {
+//   const collectionNameImage = document.getElementById(`image-${collectionNameText}`);
+//   collectionNameImage.classList.toggle("show");
+// });
+
+
+
+
+// const collectionWinter = document.querySelector('#winter-collection');
+// collectionWinter.addEventListener('click', (event) => {
+//   const winterImage = document.getElementById('image-winter');
+//   winterImage.classList.toggle("show");
+// });
+
+
+
+// const collectionName = document.querySelectorAll('.collection')[2].text.toLowerCase();
+
+
+
+
+
+// const collectionName = document.getElementById('summer-collection');
+// console.log(collectionName.text.toLowerCase());
+// collectionName.addEventListener('click', (event) => {
+//   const collectionNameText = collectionName.text.toLowerCase();
+//   const collectionNameImage = document.getElementById(`image-${collectionNameText}`);
+//   console.log(collectionNameImage);
+//   collectionNameImage.classList.toggle("show");
+// });
+
+
+
+
+
+// const collectionNameImage = document.getElementById(`image-${collectionName}`);
+// console.log(collectionNameImage);
+
+
+
+// collectionName.addEventListener('click', (event) => {
+//   const
+// });

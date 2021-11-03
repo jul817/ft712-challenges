6025d5f23c5f4b18695a402334a53fbe98d1dd62
Julie Miguel <pascalinem660@gmail.com>
2021-11-04 01:18:17 +0400

    cleaned up html and js files

--- main.js
+++ main.js
@@ -11,99 +11,80 @@ window.addEventListener("scroll", () => {
   }
 });
 
-// Collections dropdown content
-const collection = document.querySelector('#dropbtn-left');
-
-// let state = droplist.style.display;
-// droplist.style.display = "none";
-collection.addEventListener('click', (event) => {
-  // let state = droplist.style.display;
-  // let droplist = document.querySelector(".dropdown-content-left");
-  // if (droplist.style.display = "none"){
-    const dropListCollection = document.querySelector(".dropdown-content-left");
-    dropListCollection.classList.toggle("show");
-   // console.log(droplist.style.display);
-    // droplist.style.display = "flex";
-    // console.log(droplist.style.display);
- // } else {
-   // droplist.style.display = "none";
-  // console.log(droplist.style.display);
- // }
-});
-
-// const documentBody = document.body;
-// const dropListCollection = document.querySelector(".dropdown-content-left");
-// documentBody.addEventListener('click', (event) => {
-//   if (dropListCollection.classList.value.includes('show')) {
-//     dropListCollection.classList.remove('show');
-//   }
-// });
-
-
-// Contact Us dropdown content
-const contact = document.querySelector('#dropbtn-right');
-contact.addEventListener('click', (event) => {
-  const dropListContact = document.querySelector(".dropdown-content-right");
-
-    dropListContact.classList.toggle("show");
-
-});
-
-// Collection images
-
-
-
-
+// toggle to show function
 const toggleShow = () => {
   const collectionNameImage = document.getElementById(`image-${collectionNameText}`);
   collectionNameImage.classList.toggle("show");
 };
 
 
-// const collectionSummer = document.getElementById('summer-collection');
-// const collectionWinter = document.getElementById('winter-collection');
-// const collectionAutumn = document.getElementById('autumn-collection');
+
+// Collections dropdown content
+const collection = document.querySelector('#dropbtn-left');
+const dropListCollection = document.querySelector(".dropdown-content-left");
+collection.addEventListener('click', (event) => {
+  dropListCollection.classList.toggle("show");
+});
+
+// Contact Us dropdown content
+const contact = document.querySelector('#dropbtn-right');
+contact.addEventListener('click', (event) => {
+  const dropListContact = document.querySelector(".dropdown-content-right");
+  dropListContact.classList.toggle("show");
+
+});
+
+
+// Our shops dropdown content
+const shops = document.querySelector('#dropbtn-right-shops');
+const dropListShops = document.querySelector("#dropdown-content-shops");
+shops.addEventListener('click', (event) => {
+  dropListShops.classList.toggle("show");
+  // if (dropListShops.classList.value.includes('show')) {
+  //   alert('it includes');
+  // } else {
+  //   alert('it does not include');
+  // }
+}, true);
+
+
+
+
+
+// const shopList = document.querySelectorAll('.shop-link');
+// shopList.forEach(element => {
+//   element.addEventListener('mouseout', (event) => {
+//     dropListShops.classList.remove("show");
+//   }, true);
+// });
+
+
+// Add image to Collection dropdown menu
 const collectionList = document.querySelectorAll('.collection');
 const collectionImage = document.getElementById('image-autumn');
-// collectionAutumnImage.style.zIndex = 1;
-// console.log(collectionName.text.toLowerCase());
 collectionList.forEach(element => {
   const collectionName = document.getElementById(`${element.id}`);
   const collectionNameText = collectionName.text.toLowerCase();
   const collectionNameImage = document.getElementById(`image-${collectionNameText}`);
   const collectionImageValue = collectionNameImage.firstElementChild.attributes[0].value;
-  console.log(collectionImageValue);
-  // console.log(collectionName);
-  // const collectionNameText = collectionName.text.toLowerCase();
   element.addEventListener('click', (event) => {
-    // const collectionName = document.getElementById(`${element.id}`);
-    // const collectionNameText = collectionName.text.toLowerCase();
-    // const collectionNameImage = document.getElementById(`image-${collectionNameText}`);
-    console.log(collectionNameText);
     collectionImage.firstElementChild.attributes[0].value = `${collectionImageValue}`;
-    // const collectionNameImage = document.getElementById(`image-${collectionNameText}`);
-    // collectionNameImage.classList.toggle("show");
-    // collectionNameImage.style.zIndex += 1;
   });
- // const collectionNameText = collectionName.text.toLowerCase();
-   // console.log(collectionNameText);
 });
 
 
+
+// Display selected image from Collection list as banner
 const dropImage = document.querySelectorAll('.drop-image-item');
 dropImage.forEach(element => {
   element.addEventListener('click', (event) => {
     const dropImageItem = element.firstElementChild.attributes[0].value;
-    console.log(dropImageItem);
     const bannerImage = document.querySelector('.hero-banner');
     bannerImage.style.backgroundImage = `url('${dropImageItem}')`;
   });
 });
 
-
-
-
-
+// Add email address as account status
 const button = document.querySelector('.submit-button');
 button.addEventListener('click', (event) => {
   event.preventDefault();
@@ -111,91 +92,21 @@ button.addEventListener('click', (event) => {
   const accountEmail = document.querySelector('.account');
   accountEmail.textContent = emailInput.value;
   window.location.assign('localhost:8000/email');
-
-  emailInput.value ="";
+  emailInput.value = "";
 });
 
 
-// const documentBody = document.body;
+  // const documentBody = document.querySelector('body');
+// documentBody.removeEventListener('click', (event) => {
+//   dropListShops.classList.toggle("show");
+// }, true);
+
+// const documentBody = document.querySelector('body');
 // documentBody.addEventListener('click', (event) => {
-//   if(dropListCollection.classList.value.includes('show')){
-//     dropListCollection.classList.remove('show');
+//   // const allDropDowns = document
+//   if (dropListShops.classList.value('show')) {
+//     dropListShops.classList.remove('show');
+//   } else {
+//     dropListShops.classList.add('show');
 //   }
-// });
-
-
-// const dropImageItem = dropImage.firstElementChild.attributes[0].value;
-// console.log(dropImageItem);
-// dropImage.addEventListener('click', (event) => {
-//   const bannerImage = document.querySelector('.hero-banner');
-//   bannerImage.style.backgroundImage = `url('${dropImageItem}')`;
-//   console.log(bannerImage);
-// });
-
-
-// collectionName.addEventListener('click', toggleShow);
-// const collectionNameText = collectionName.text.toLowerCase();
-
-
-
-// console.log(collectionNameText);
-
-
-
-// => {
-//   const collectionNameImage = document.getElementById(`image-${collectionNameText}`);
-//   collectionNameImage.classList.toggle("show");
-// });
-
-
-
-
-// const collectionWinter = document.querySelector('#winter-collection');
-// collectionWinter.addEventListener('click', (event) => {
-//   const winterImage = document.getElementById('image-winter');
-//   winterImage.classList.toggle("show");
-// });
-
-
-
-// const collectionName = document.querySelectorAll('.collection')[2].text.toLowerCase();
-
-
-
-
-
-// const collectionName = document.getElementById('summer-collection');
-// console.log(collectionName.text.toLowerCase());
-// collectionName.addEventListener('click', (event) => {
-//   const collectionNameText = collectionName.text.toLowerCase();
-//   const collectionNameImage = document.getElementById(`image-${collectionNameText}`);
-//   console.log(collectionNameImage);
-//   collectionNameImage.classList.toggle("show");
-// });
-
-
-
-
-
-// const collectionNameImage = document.getElementById(`image-${collectionName}`);
-// console.log(collectionNameImage);
-
-
-
-// collectionName.addEventListener('click', (event) => {
-//   const
-// });
-
-
-
-
-
-
-// const dropImage = document.querySelector('#image-winter');
-// const dropImageItem = dropImage.firstElementChild.attributes[0].value;
-// console.log(dropImageItem);
-// dropImage.addEventListener('click', (event) => {
-//   const bannerImage = document.querySelector('.hero-banner');
-//   bannerImage.style.backgroundImage = `url('${dropImageItem}')`;
-//   console.log(bannerImage);
 // });
